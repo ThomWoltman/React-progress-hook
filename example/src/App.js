a693f2918eadefa3f7c9b6de911d9dec9cd12640
Thom Woltman <t-woltman@hotmail.com>
2020-03-02 01:06:16 +0100

    example

---
+++ example/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+
+import { useFakeLoaderState } from "React-progress-hook";
+
+const App = () => {
+  const { displayPercentage, isLoading, onProgress } = useFakeLoaderState({
+    exptedStepTime: 2000,
+    digits: 0
+  });
+
+  const onClick = async e => {
+    onProgress({ total: 4, done: 0 });
+    for (let i = 1; i <= 4; i++) {
+      await timeoutAsync(2000);
+      onProgress({ total: 4, done: i });
+    }
+  };
+
+  function timeoutAsync(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  return (
+    <div>
+      {!isLoading && <button onClick={onClick}>load</button>}
+      {isLoading && <h1>{displayPercentage}%</h1>}
+    </div>
+  );
+};
+export default App;
